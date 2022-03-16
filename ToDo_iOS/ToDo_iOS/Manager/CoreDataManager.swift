537ad7645e356793ec88f180dd4e962211f6e248
Poonam mahi <poonammahi379@gmail.com>
2022-03-16 12:23:58 -0400

    created datamodel and a swift file to set coredata stack

---
+++ ToDo_iOS/ToDo_iOS/Manager/CoreDataManager.swift
@@ -0,0 +1,9 @@
+//
+//  CoreDataManager.swift
+//  ToDo_iOS
+//
+//  Created by Poonam Mahi on 2022-03-16.
+//
+
+import Foundation
+//responsible for setting coredata stack
---
+++ ToDo_iOS/ToDo_iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDo_iOS
+//
+//  Created by Poonam Mahi on 2022-03-16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
